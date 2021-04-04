be6cc0c09d90c5cee402691fa1fc8092f63bb20e
Stanko172 <stanko.bebek82@gmail.com>
2021-04-04 17:55:22 +0200

    added autoload function

---
+++ admin/includes/functions.php
@@ -0,0 +1,11 @@
+<?php
+
+    function __autoload($class){
+        $class = strtolower($class);
+
+        $file_path = "includes/$class.php";
+
+        !file_exists($file_path) ? die("File $class.php does not exist!") : require_once($file_path);
+    }
+
+?>
