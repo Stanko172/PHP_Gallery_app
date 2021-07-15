3fc596f7eb20a13a42f0a0c71422e5e975b1544b
Stanko172 <stanko.bebek00@gmail.com>
2021-07-15 14:24:14 +0200

    finished index.php

--- index.php
+++ index.php
@@ -1,26 +1,32 @@
 <?php include("includes/header.php"); ?>
 
+<?php  
+
+    $photos = Photo::get_all();
+
+?>
 
         <div class="row">
 
             <!-- Blog Entries Column -->
-            <div class="col-md-8">
+            <?php foreach($photos as $photo) : ?>
+            <div class="col-md-4" style="margin-top: 25px;">
 
     
             
-          
+            <div class="card" style="width: 250px;">
+                <img class="card-img-top" src="<?php echo "admin" . DS . $photo->image_path(); ?>" alt="Card image cap" width="250" height="250">
+                <div class="card-body">
+                    <h3 class="card-title text-center"><?php echo $photo->title; ?></h3>
+                    <a href="photo.php?id=<?php echo $photo->id;?>" class="btn btn-primary btn-block">Opširnije</a>
+                </div>
+            </div>
          
 
             </div>
 
+            <?php endforeach; ?>
 
-
-
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-            
-                 <?php include("includes/sidebar.php"); ?>
 
 
 
@@ -28,3 +34,11 @@
         <!-- /.row -->
 
         <?php include("includes/footer.php"); ?>
+
+<style>
+.card {
+        margin: 0 auto; /* Added */
+        float: none; /* Added */
+        margin-bottom: 10px; /* Added */
+}
+</style>
---
+++ init.php
@@ -0,0 +1,13 @@
+<?php
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+
+require_once("admin/includes/functions.php");
+require_once("admin/includes/new_config.php");
+require_once("admin/includes/database.php");
+require_once("admin/includes/db_object.php");
+require_once("admin/includes/photo.php");
+require_once("admin/includes/session.php");
+require_once("admin/includes/user.php");
+require_once("admin/includes/comment.php");
+
+?>
