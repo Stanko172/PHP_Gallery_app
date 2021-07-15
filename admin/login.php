b3645f796d80c404bf2e9c4df651ac3d5156753a
Stanko172 <stanko.bebek00@gmail.com>
2021-07-15 15:43:13 +0200

    added frontend for login page

--- admin/login.php
+++ admin/login.php
@@ -29,34 +29,145 @@ if(isset($_POST['submit'])){
 
 ?>
 
+<link href='https://fonts.googleapis.com/css?family=Open+Sans:700,600' rel='stylesheet' type='text/css'>
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js" type="text/javascript"></script>
+<script>
+    function field_focus(field, email)
+  {
+    if(field.value == email)
+    {
+      field.value = '';
+    }
+  }
+
+  function field_blur(field, email)
+  {
+    if(field.value == '')
+    {
+      field.value = email;
+    }
+  }
+
+//Fade in dashboard box
+$(document).ready(function(){
+    $('.box').hide().fadeIn(1000);
+    });
+
+//Stop click event
+$('a').click(function(event){
+    event.preventDefault(); 
+	});
+</script>
+<style>
+body{
+  font-family: 'Open Sans', sans-serif;
+  background:#3498db;
+  margin: 0 auto 0 auto;  
+  width:100%; 
+  text-align:center;
+  margin: 20px 0px 20px 0px;   
+}
+
+p{
+  font-size:12px;
+  text-decoration: none;
+  color:#ffffff;
+}
+
+h1{
+  font-size:1.5em;
+  color:#525252;
+}
+
+.box{
+  background:white;
+  width:300px;
+  border-radius:6px;
+  margin: 0 auto 0 auto;
+  padding:0px 0px 70px 0px;
+  border: #2980b9 4px solid; 
+}
+
+.email{
+  background:#ecf0f1;
+  border: #ccc 1px solid;
+  border-bottom: #ccc 2px solid;
+  padding: 8px;
+  width:250px;
+  color:#AAAAAA;
+  margin-top:10px;
+  font-size:1em;
+  border-radius:4px;
+}
+
+.password{
+  border-radius:4px;
+  background:#ecf0f1;
+  border: #ccc 1px solid;
+  padding: 8px;
+  width:250px;
+  font-size:1em;
+}
+
+.btn{
+  background:#2ecc71;
+  width:125px;
+  padding-top:5px;
+  padding-bottom:5px;
+  color:white;
+  border-radius:4px;
+  border: #27ae60 1px solid;
+  
+  margin-top:20px;
+  margin-bottom:20px;
+  float:left;
+  margin-left:16px;
+  font-weight:800;
+  font-size:0.8em;
+}
+
+.btn:hover{
+  background:#2CC06B; 
+}
+
+#btn2{
+  float:left;
+  background:#3498db;
+  width:125px;  padding-top:5px;
+  padding-bottom:5px;
+  color:white;
+  border-radius:4px;
+  border: #2980b9 1px solid;
+  
+  margin-top:20px;
+  margin-bottom:20px;
+  margin-left:10px;
+  font-weight:800;
+  font-size:0.8em;
+}
+
+#btn2:hover{ 
+background:#3594D2; 
+}
+</style>
+
 <h4 class="bg-danger"><?php echo $msg; ?></h4>
 
-<div class="col-md-4 col-md-offset-3">
+<form method="POST" action="login.php">
+<div class="box">
+<h1>GalleryApp</h1>
 
-<form id="login-id" action="" method="post">
-	
-<div class="form-group">
-	<label for="username">Username</label>
-	<input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
+<input type="text" name="username" value="<?php echo htmlentities($username); ?>" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+  
+<input type="password" name="password" value="<?php echo htmlentities($password); ?>" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+  
+<button class="btn" type="submit" name="submit">Sign In</button> <!-- End Btn -->
 
-</div>
-
-<div class="form-group">
-	<label for="password">Password</label>
-	<input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
-	
-</div>
-
-
-<div class="form-group">
-<input type="submit" name="submit" value="Submit" class="btn btn-primary">
-
-</div>
-
-
+<a href="#"><div id="btn2">Sign Up</div></a> <!-- End Btn2 -->
+  
+</div> <!-- End Box -->
+  
 </form>
 
-</div>
-
-
-</div>
+<p>Forgot your password? <u style="color:#f1c40f;">Click Here!</u></p>
